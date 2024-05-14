bebd487e5b9dbf06042ba4d5d5432e6844ec7b28
smiley88888 <wannarichman88888@gmail.com>
2024-05-14 11:36:43 -0400

    add: upload xlsx sheets to qdrant

---
+++ uploadQdrant.py
@@ -0,0 +1,37 @@
+import os
+import pandas as pd
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Qdrant
+import logging
+from config import config
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s: %(message)s', datefmt='%Y-%m-%d %I:%M:%S', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+qdrant_url=config["QDRANT_URL"]
+qdrant_api_key=config["QDRANT_API_KEY"]
+
+# embeddings = OpenAIEmbeddings()
+embeddings = HuggingFaceEmbeddings(model_name="intfloat/multilingual-e5-base")
+
+
+def processExcel(file_path):
+    # Create an ExcelFile object
+    xlsx = pd.ExcelFile(file_path)
+
+    # List all sheet names
+    sheet_names = xlsx.sheet_names
+    print("Sheet names:", sheet_names)
+
+    for sheet_name in sheet_names:
+        df = pd.read_excel(file_path, sheet_name=sheet_name)
+        docs = df.iloc[:, 0].tolist()
+        doc_store = Qdrant.from_texts(texts=docs, embedding=embeddings, url=qdrant_url, api_key=qdrant_api_key, collection_name=sheet_name, timeout=1000)
+        print(doc_store)
+        
+
+if __name__ == "__main__":
+    processExcel("data/Data for clustering (EverGrowing).xlsx")
+
